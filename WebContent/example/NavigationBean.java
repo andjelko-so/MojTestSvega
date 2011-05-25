f8e788f6e56e509530092292a64da7a69789cf9e
Milos Djikic <djikicmilos11@gmail.com>
2011-05-25 11:45:13 +0200

    MojTestSvega commit message

---
+++ WebContent/example/NavigationBean.java
@@ -0,0 +1,51 @@
+package rs.paragraf.web.example;
+
+import java.io.*;
+import java.util.*;
+import javax.faces.bean.*;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.*;
+import javax.faces.event.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpSession;
+
+@ManagedBean
+@SessionScoped
+public class NavigationBean implements Serializable {
+
+	private String pageHeight;
+
+	public String getPageHeight() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		HttpServletRequest myRequest = (HttpServletRequest)context.getExternalContext().getRequest();
+		HttpSession mySession = myRequest.getSession();
+		myRequest.getParameter("webParagrafPaneldividerHeight");
+		pageHeight = new String("webParagrafPaneldividerHeight");
+		return pageHeight;
+	}
+
+	public void setPageHeight(String pageHeight) {
+		this.pageHeight = pageHeight;
+	}
+
+	public String getStyle(){
+		String style = String.format("width: 100%; height: %s", pageHeight);
+		return(style);
+	}
+
+	public String doNav(){
+		String retVal = null;
+		FacesContext context = FacesContext.getCurrentInstance();
+		Map<String, Object> requests = context.getExternalContext().getRequestMap();
+		for (String requestName : requests.keySet()) {
+		if (requests.get(requestName) instanceof HttpServletRequestWrapper) {
+		HttpServletRequest request = (HttpServletRequest) ((HttpServletRequestWrapper) requests.get(requestName)).getRequest();
+		retVal = request.getParameter("webParagrafPaneldividerHeight");
+		}
+		}
+	    //pageHeight = (String) FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("webParagrafPaneldividerHeight");
+		pageHeight = retVal;
+		return("testSplittera");
+	}
+}
---
+++ WebContent/resources/scripts/simpleSnippetTransitionScript.js
@@ -0,0 +1,12 @@
+var loadedobjects=""
+
+function changeElementValue()
+{
+	document.getElementById("north").style.display = "none";
+}
+
+function changeElementValue1()
+{
+	document.getElementById("north").style.display = "block";
+}
+
